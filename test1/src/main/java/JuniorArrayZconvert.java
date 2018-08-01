2bc6af7bf8f6e250327e945a0a13f11db68ebc46
wangqi <wangqi-bj@ruiyinxin.com>
2018-08-01 11:27:37 +0800

    update: conclusion

--- test1/src/main/java/JuniorArrayZconvert.java
+++ test1/src/main/java/JuniorArrayZconvert.java
@@ -9,20 +9,35 @@
  */
 
 public class JuniorArrayZconvert {
-    public  static  String convert(String s, int numRows) {
-        int length = numRows-2;
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        //外部规律numRows-2
+        int length = numRows - 2;
+        //内部规律2*numRows-4
+        int length2 = (numRows << 1) - 4;
+        boolean isAdd = false;
         StringBuffer ss = new StringBuffer();
-        for(int i=0;i<numRows;i++){
-            int num= i;
-            while (num<s.length()){
+        for (int i = 0; i < numRows; i++) {
+            int num = i;
+            while (num < s.length()) {
                 ss.append(s.charAt(num));
-                num += numRows+length;
+                if (length2 > 0 && i != 0 && i != numRows - 1) {
+                    isAdd = true;
+                    if ((num + length2) < s.length()) {
+                        ss.append(s.charAt(num + length2));
+                    }
+                }
+                num += numRows + length;
+            }
+            if (isAdd) {
+                length2 -= 2;
             }
         }
         return ss.toString();
     }
 
-    public static void main(String[] args){
-        System.out.println(convert("PAYPALISHIRING",4));
+    public static void main(String[] args) {
+        System.out.println(4 << 1);
+        System.out.println(convert("ABCDE", 4));
     }
 }
